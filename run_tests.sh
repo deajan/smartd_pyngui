e882d920ebb50f36064de8bc935af9dd90f0fa4f
deajan <ozy@netpower.fr>
2019-01-02 17:18:53 +0100

    Use xvfb for travis

--- run_tests.sh
+++ run_tests.sh
@@ -10,33 +10,44 @@ echo using basedir [$BASE_DIR]
 # Arguments: 1= Pid to wait for, 2= timeout (s)
 function WaitForIt {
 	local start=$SECONDS
-	while ps -p $1 > /dev/null 2>&1
+	local pid=${1}
+	local timeout=${2}
+
+	local result
+
+	while ps -p $pid > /dev/null 2>&1
 	do
 		sleep 1
-		if [ $((SECONDS - start)) -ge $2 ]; then
-			kill -9 $1
+		if [ $((SECONDS - start)) -ge $timeout ]; then
+			kill -9 $pid
 		fi
 	done
-	wait $1
-	return $?
+	wait $pid
+	result=$?
+	return $result
 }
 
 find "$BASE_DIR" -name "*.pyc" -or -name "__pycache__" | xargs -I {} rm -rf {}
 
 export PYTHONPATH=$BASE_DIR
-export DISPLAY=:0.0
+#export DISPLAY=:0.0
 
 cd "$BASE_DIR" || (echo "Cannot switch to directory [$BASE_DIR]." && exit 1)
 
-python smartd_pyngui/smartd_pyngui.py &
+if type python3 >/dev/null 2>&1; then
+	python3 smartd_pyngui/smartd_pyngui.py &
+else
+	python smartd_pyngui/smartd_pyngui.py &
+fi
 WaitForIt $! 20
+result=$?
 
-if [ $? == 137 ]; then
+if [ $result == 137 ]; then
 	echo "App launched from shell (seems) okay (137 = kill signal after 20 seconds)."
 else
-	echo "Cannot launch app from shell."
+	echo "Cannot launch app from shell (exit code: $result)."
 	exit 1
 fi
 
 # py.test currently failing
-#py.test
+py.test
