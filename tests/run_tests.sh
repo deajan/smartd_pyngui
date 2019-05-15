46752bdf355955fa8b8e5f00d28ffde2514657b9
Orsiris de Jong <ozy@netpower.fr>
2019-05-15 13:38:22 +0200

    Updated tests with shell run and nuitka compilation

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -1,9 +1,60 @@
 #!/usr/bin/env bash
 
-# Worst travis test script ever !
-
 BASE_DIR="$(pwd)"
 
 echo using basedir [$BASE_DIR]
 
+# Arguments: 1= Pid to wait for, 2= timeout (s)
+function WaitForIt {
+	local start=$SECONDS
+	local pid=${1}
+	local timeout=${2}
+
+	local result
+
+	while ps -p $pid > /dev/null 2>&1
+	do
+		sleep 1
+		if [ $((SECONDS - start)) -ge $timeout ]; then
+			kill -9 $pid
+		fi
+	done
+	wait $pid
+	result=$?
+	return $result
+}
+
+cd "$BASE_DIR" || (echo "Cannot switch to directory [$BASE_DIR]." && exit 1)
+
+# Make sure we don't run on any forms of cache
+find "$BASE_DIR" -name "*.pyc" -or -name "__pycache__" | xargs -I {} rm -rf {}
+
+# First let's check if program can run
+"python $BASE_DIR/smartd_pyngui/smartd_pyngui.py" &
+WaitForIt $! 20
+result=$?
+
+if [ $result -eq 137 ]; then
+    echo "App launched from shell with success. Exit code [$result] is kill signal."
+else
+    echo "App failed to launch from shell. Exit code [$result]."
+    exit $result
+fi
+
+# Run unit tests
 py.test
+
+# Try nuitka compilation
+python -m nuitka --standalone "$BASE_DIR/smartd_pyngui/smartd_pyngui.py"
+result=$?
+
+if [ $result -ne 0 ]; then
+    echo "Nuitka compilation failed. Exit code [$result]."
+    exit $result
+else
+    echo "Nuitka compilation success. Trying to run built software."
+    "$BASE_DIR/smartd_pyngui/smartd_pyngui.dist/smartd_pyngui" &
+    WaitForIt $! 20
+    result=$?
+    echo "Nuitka compiled app exited with code [$result]."
+fi
