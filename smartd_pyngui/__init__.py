77e8c1472cd83e6944c8d8f86766598ddbbdf13d
Orsiris de Jong <ozy@netpower.fr>
2019-05-15 12:38:22 +0200

    Made smartd_pyngui become a packakge for tests

--- smartd_pyngui/__init__.py
+++ smartd_pyngui/__init__.py
@@ -2,5 +2,10 @@
 #
 #   This is a dummy file to make the following directory a package
 
-name = "smartd_pyngui"
+import sys
+import os
+
+# Fix so smartd_pyngui can be called outside from its directory (eg tests)
+sys.path.insert(0, os.path.dirname(__file__))
+
 from .smartd_pyngui import *
