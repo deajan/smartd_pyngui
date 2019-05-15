b5974c062810d3b143d868fe2c2c493a519edde6
Orsiris de Jong <ozy@netpower.fr>
2019-05-15 14:27:13 +0200

    Add empty unit test templates

--- tests/test_all.py
+++ tests/test_all.py
@@ -8,14 +8,37 @@ from smartd_pyngui.smartd_pyngui import *
 
 config = Configuration()
 
+
 def test_load_app():
     assert APP_VERSION is not None
     assert os.path.isdir(config.app_root) is True
     assert os.path.isfile(config.app_executable) is True
 
-#def test_load_app():
-#    ret = main('--test')
-#    assert ret == 0
+
+def test_read_smartd_conf_file():
+    pass
+
+
+def test_write_smartd_conf_file():
+    pass
+
+
+def test_read_alert_conf_file():
+    pass
+
+
+def test_write_alert_conf_file():
+    pass
+
+
+def test_compare_smartd_conf_files():
+    pass
+
+
+def test_compare_alert_conf_files():
+    pass
+
+
 
 # TODO Write smartd & alert conf file RW tests
 # smartd conf file needs to be compared with basic one
