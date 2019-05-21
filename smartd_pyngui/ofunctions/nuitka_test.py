deajan/smartd_pyngui

commit 6fdbc992b9d7b06c3b16adff594fa328713a45a0

Multiple lint fixes

smartd_pyngui/ofunctions/ofunctions.py
@@ -185,13 +185,14 @@ def bytes_to_string(bytes_to_convert):
         return False
 
 
-def command_runner(command, valid_exit_codes=[], timeout=30, shell=False, decoder='utf-8'):
+def command_runner(command, valid_exit_codes=None, timeout=30, shell=False, decoder='utf-8'):
     """
     command_runner 2019011001
     Whenever we can, we need to avoid shell=True in order to preseve better security
     Runs system command, returns exit code and stdout/stderr output, and logs output on error
     valid_exit_codes is a list of codes that don't trigger an error
     """
+
     try:
         # universal_newlines=True makes netstat command fail under windows
         # timeout may not work on linux
@@ -208,9 +209,9 @@ def command_runner(command, valid_exit_codes=[], timeout=30, shell=False, decode
             except Exception as subexc:
                 logger.debug(subexc, exc_info=True)
                 logger.debug('Cannot properly decode error. Text is %s' % output)
-        except:
+        except Exception:
             output = "command_runner: Could not obtain output from command."
-        if exit_code in valid_exit_codes:
+        if exit_code in valid_exit_codes if valid_exit_codes is not None else []:
             logger.debug('Command [%s] returned with exit code [%s]. Command output was:' % (command, exit_code))
             if output:
                 logger.debug(output)
@@ -346,26 +347,26 @@ def check_for_virtualization(product_id):
     Xen adds 'Xen' to Win32_BIOS.Version (well hopefully)
     """
 
-    for id in product_id:
+    for p_id in product_id:
         # First try to detect oVirt before detecting Qemu/KVM
-        if re.search('oVirt', id, re.IGNORECASE):
+        if re.search('oVirt', p_id, re.IGNORECASE):
             return True, 'oVirt'
-        elif re.search('VBOX', id, re.IGNORECASE):
+        elif re.search('VBOX', p_id, re.IGNORECASE):
             return True, 'VirtualNox'
-        elif re.search('VMWare', id, re.IGNORECASE):
+        elif re.search('VMWare', p_id, re.IGNORECASE):
             return True, 'VMWare'
-        elif re.search('Hyper-V', id, re.IGNORECASE):
+        elif re.search('Hyper-V', p_id, re.IGNORECASE):
             return True, 'Hyper-V'
-        elif re.search('Xen', id, re.IGNORECASE):
+        elif re.search('Xen', p_id, re.IGNORECASE):
             return True, 'Xen'
-        elif re.search('KVM', id, re.IGNORECASE):
+        elif re.search('KVM', p_id, re.IGNORECASE):
             return True, 'KVM'
-        elif re.search('qemu', id, re.IGNORECASE):
+        elif re.search('qemu', p_id, re.IGNORECASE):
             return True, 'qemu'
-        elif re.search('bochs', id, re.IGNORECASE):
+        elif re.search('bochs', p_id, re.IGNORECASE):
             return True, 'bochs'
         # Fuzzy detection
-        elif re.search('VRTUAL', id, re.IGNORECASE):
+        elif re.search('VRTUAL', p_id, re.IGNORECASE):
             return True, 'HYPER-V'
     return False, 'Physical / Unknown hypervisor'
 

smartd_pyngui/ofunctions/FileUtils.py
@@ -10,6 +10,8 @@ from fnmatch import fnmatch
 from contextlib import contextmanager
 from threading import Lock
 
+BUILD = '2019052101'
+
 file_lock_token = None
 
 
@@ -55,7 +57,7 @@ def glob_path_match(path, pattern_list):
     return any(fnmatch(os.path.basename(path), pattern) for pattern in pattern_list)
 
 
-def get_files_recursive(root, d_exclude_list=[], f_exclude_list=[], ext_exclude_list=[], primary_root=None):
+def get_files_recursive(root, d_exclude_list=None, f_exclude_list=None, ext_exclude_list=None, primary_root=None):
     """
     Walk a path to recursively find files
     Modified version of https://stackoverflow.com/a/24771959/2635443 that includes exclusion lists
@@ -68,8 +70,15 @@ def get_files_recursive(root, d_exclude_list=[], f_exclude_list=[], ext_exclude_
     :return: list of files found in path
     """
 
-    # Make sure we use a valid os separator for exclusion lists, this is done recursively :(
-    d_exclude_list = [os.path.normpath(d) for d in d_exclude_list]
+    if d_exclude_list is not None:
+        # Make sure we use a valid os separator for exclusion lists, this is done recursively :(
+        d_exclude_list = [os.path.normpath(d) for d in d_exclude_list]
+    else:
+        d_exclude_list = []
+    if f_exclude_list is None:
+        f_exclude_list = []
+    if ext_exclude_list is None:
+        ext_exclude_list = []
 
     files = [os.path.join(root, f) for f in os.listdir(root) if os.path.isfile(os.path.join(root, f))
              and not glob_path_match(f, f_exclude_list) and os.path.splitext(f)[1] not in ext_exclude_list]
@@ -138,7 +147,7 @@ def remove_files_older_than(days, directory):
     if not os.path.isdir(directory):
         raise FileNotFoundError('[%s] not found.' % directory)
     else:
-        for root, dirnames, filenames in os.walk(directory):
+        for root, _, filenames in os.walk(directory):
             for filename in filenames:
                 if os.path.isfile(filename) and (
                         ((time.time() - (days * 86400)) - file_creation_date(filename)) < 0):

smartd_pyngui/ofunctions/Mailer.py
@@ -45,7 +45,7 @@ def send_email(source_mail=None, destination_mails=None, split_mails=False, smtp
 
     if destination_mails is None:
         raise ValueError('No destination mails set')
-    elif type(destination_mails) is not list:
+    elif isinstance(list, destination_mails):
         # Make sure destination mails is a list
         destination_mails = destination_mails.split(' ')
 

smartd_pyngui/smartd_pyngui.py
@@ -23,7 +23,6 @@ if sys.argv[0].endswith(".exe") or sys.argv[0].endswith(".EXE"):
     os.environ['TCL_LIBRARY'] = os.path.abspath(os.path.dirname(sys.argv[0])) + os.sep + 'tcl'
     os.environ['TK_LIBRARY'] = os.path.abspath(os.path.dirname(sys.argv[0])) + os.sep + 'tk'
 
-import tkinter
 import PySimpleGUI as sg
 
 # Module pywin32
@@ -248,7 +247,7 @@ class Configuration:
                             config_list = line.split(' -')
                             config_list = [config_list[0]] + ['-' + item for item in config_list[1:]]
                             # Remove unnecessary blanks and newlines
-                            for i, item in enumerate(config_list):
+                            for i, _ in enumerate(config_list):
                                 config_list[i] = config_list[i].strip()
                             drive_list.append(config_list[0])
                             del config_list[0]
@@ -586,6 +585,7 @@ class MainGuiApp:
             self.window.Element('mail_addresses').Update(disabled=True)
             self.window.Element('external_script_path').Update(disabled=False)
 
+    @staticmethod
     def spacer_tweakf(self, pixels=10):
         return [sg.T(' ' * pixels, font=('Helvetica', 1))]
 
@@ -738,7 +738,7 @@ class MainGuiApp:
 
         # smartd health parameters
         try:
-            for key, description in self.health_parameter_map:
+            for key, _ in self.health_parameter_map:
                 if values[key]:
                     # Handle dependancies
                     if key == '-C 197+':
@@ -759,11 +759,7 @@ class MainGuiApp:
                         config_list.append(key)
 
         except KeyError:
-            try:
-                key
-                msg = "Bogus configuration in [%s]." % key
-            except:
-                msg = "Bogus configuration in health parameters."
+            msg = "Bogus configuration in health parameters."
             logger.error(msg)
             logger.debug('Trace:', exc_info=True)
             logger.debug(config_list)
@@ -883,6 +879,7 @@ class MainGuiApp:
         self.config.config_list = config_list
         return True
 
+    @staticmethod
     def service_reload(self):
         try:
             system_service_handler(SMARTD_SERVICE_NAME, "restart")
@@ -913,7 +910,7 @@ class MainGuiApp:
                                         text_justification='left', layout=layout)
 
             while True:
-                event, values = raw_view_window.Read(timeout=1000)
+                event, _ = raw_view_window.Read(timeout=1000)
                 if event == 'Back' or event is None:
                     raw_view_window.Close()
                     break
@@ -1132,7 +1129,7 @@ def system_service_handler(service, action):
         # service_status = os.system("service " + service + " status > /dev/null 2>&1")
 
         # Valid exit code are 0 and 3 (because of systemctl using a service redirect)
-        service_status, service_output = ofunctions.command_runner('service %s status' % service)
+        service_status, _ = ofunctions.command_runner('service %s status' % service)
         if service_status == 0:
             is_running = True
         else:
@@ -1152,6 +1149,7 @@ def system_service_handler(service, action):
                         return True
                     else:
                         logger.error('Could not start service, code [%s].' % result)
+                        logger.error('Output: %s' % output)
                         raise Exception
                 except Exception:
                     logger.info(msg_failure)
@@ -1171,6 +1169,7 @@ def system_service_handler(service, action):
                         return True
                     else:
                         logger.error('Could not start service, code [%s].' % result)
+                        logger.error('Output: %s' % output)
                         raise Exception
                 except Exception:
                     logger.error(msg_failure)
@@ -1231,8 +1230,9 @@ def trigger_alert(config, mode=None):
             command = 'smartctl -a /dev/pd0'
             exit_code, output = ofunctions.command_runner(command)
             attachment = zlib.compress(output.encode('utf-8'))
-        except:
+        except Exception:
             logger.error('Cannot get smartctl output.')
+            logger.debug('Trace', exc_info=True)
             attachment = None
 
         if len(src) > 0 and len(dst) > 0 and len(smtp_server) > 0 and len(smtp_port) > 0:
@@ -1242,7 +1242,8 @@ def trigger_alert(config, mode=None):
                                                    smtp_user=smtp_user, smtp_password=smtp_password, security=security,
                                                    subject=subject, attachment=attachment, filename='log.zip',
                                                    body=warning_message, debug=True) # TODO remove debug True
-
+                # WIP
+                logger.info('Mailer result [%s].' % ret)
             # TODO smartctl output and env variables is needed to decorate error messages and get valid smartctl output
             # TODO see why we'd favor ret over exception
 

commit abf41507804afafe3ea386512d3683de957147b1

Fix nuitka compilation regression from 6fdbc99

smartd_pyngui/ofunctions/ofunctions.py
@@ -193,6 +193,9 @@ def command_runner(command, valid_exit_codes=None, timeout=30, shell=False, deco
     valid_exit_codes is a list of codes that don't trigger an error
     """
 
+    if valid_exit_codes is None:
+        valid_exit_codes = []
+
     try:
         # universal_newlines=True makes netstat command fail under windows
         # timeout may not work on linux
@@ -211,7 +214,7 @@ def command_runner(command, valid_exit_codes=None, timeout=30, shell=False, deco
                 logger.debug('Cannot properly decode error. Text is %s' % output)
         except Exception:
             output = "command_runner: Could not obtain output from command."
-        if exit_code in valid_exit_codes if valid_exit_codes is not None else []:
+        if exit_code in valid_exit_codes:
             logger.debug('Command [%s] returned with exit code [%s]. Command output was:' % (command, exit_code))
             if output:
                 logger.debug(output)

smartd_pyngui/ofunctions/nuitka_test.py
@@ -1,0 +1,8 @@
+def test(list=None):
+    if 'element' in list if list is not None else []:
+        print('yes')
+    else:
+        print('no')
+
+test(['some', 'element'])
+test()