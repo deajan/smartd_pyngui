30ede68d4ab8b760aec5abeae9ad6927f3fd42f2
Orsiris de Jong <ozy@netpower.fr>
2020-04-12 18:41:33 +0200

    Update aes key to use revac

--- smartd_pyngui/aes_key.py
+++ smartd_pyngui/aes_key.py
@@ -1,4 +1,4 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
 
-AES_ENCRYPTION_KEY = b'bXtdBq76pAIPDGN8mxCJq5NarjO_HG5pr6CpnUf-fCg='
+AES_ENCRYPTION_KEY = b'6EhBiAmOiI5rvaUiHlqT7VORG2wzpcmekZcM4kheshM='
