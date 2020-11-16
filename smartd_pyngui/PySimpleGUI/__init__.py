8fc5644d63f993a18a1327245afc950967765a91
Orsiris de Jong <ozy@netpower.fr>
2020-11-16 13:24:26 +0100

    Update upstream PySimpleGUI from v4.14.0 to v4.31.0

--- smartd_pyngui/PySimpleGUI/__init__.py
+++ smartd_pyngui/PySimpleGUI/__init__.py
@@ -1,12 +1,2 @@
-#   Copyright 2018-2019 by Orsiris de Jong - contact@netpower.fr
-#
-#   This is a dummy file to make the following directory a package
-
-import sys
-
-if sys.version_info[0] >= 3:
-    from .PySimpleGUI import *
-else:
-    from .PySimpleGUI27 import *
-
-
+name = "PySimpleGUI"
+from .PySimpleGUI import *
